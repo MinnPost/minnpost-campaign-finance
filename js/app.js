7c9d75f3204afe34336904976dd3475dc4f845e4
Alan Palazzolo <apalazzolo@minnpost.com>
2014-01-08 14:57:26 -0600

    Basic yoemon template created.

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Main application file for: minnpost-campaign-finance
+ *
+ * This pulls in all the parts
+ * and creates the main object for the application.
+ */
+define('minnpost-campaign-finance', ['underscore', 'helpers', 'routers'],
+  function(_, helpers, routers) {
+
+  // Constructor for app
+  var App = function(options) {
+    this.options = options;
+    this.el = this.options.el;
+    if (this.el) {
+      this.$el = $(this.el);
+    }
+  };
+
+  // Extend with helpers
+  _.extend(App.prototype, helpers);
+
+  // Start function
+  App.prototype.start = function() {
+    // Create router
+    this.router = new routers.Router({
+      app: this
+    });
+
+    // Start backbone history
+    this.router.start();
+  };
+
+  return App;
+});
---
+++ js/config.js
@@ -0,0 +1,34 @@
+/**
+ * RequireJS config which maps out where files are and shims
+ * any non-compliant libraries.
+ */
+require.config({
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'Backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    // Mapbox and requireJS don't really work, so we just let
+    // the L be global
+    'mapbox': {
+      exports: 'mapbox'
+    }
+  },
+  baseUrl: 'js',
+  paths: {
+    
+    'requirejs': '../bower_components/requirejs/require',
+    'text': '../bower_components/text/text',
+    'jquery': '../bower_components/jquery/jquery.min',
+    'underscore': '../bower_components/underscore/underscore-min',
+    'Backbone': '../bower_components/backbone/backbone-min',
+    'Ractive': '../bower_components/ractive/build/Ractive-legacy.min',
+    'Ractive-Backbone': '../bower_components/ractive-backbone/Ractive-Backbone.min',
+    'Ractive-events-tap': '../bower_components/ractive-events-tap/Ractive-events-tap.min',
+    'moment': '../bower_components/moment/min/moment.min',
+    'minnpost-campaign-finance': 'app'
+  }
+});
