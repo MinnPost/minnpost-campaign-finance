f90551c1f51a450360bf122cf2545b2f949a4915
Alan Palazzolo <apalazzolo@minnpost.com>
2014-01-30 14:46:02 -0600

    Updating build.

--- dist/minnpost-campaign-finance.latest.js
+++ dist/minnpost-campaign-finance.latest.js
@@ -587,7 +587,7 @@ define('minnpost-campaign-finance', [
     // Because of Impaq.me and their use of respond.js, the Highcharts
     // don't work out.
     disableCharts: function() {
-      return (!_.isUndefined(window.impaq) && _.isObject(impaq) && helpers.isMSIE === 8);
+      return (!_.isUndefined(window.impaq) && _.isObject(impaq) && helpers.isMSIE() === 8);
     }
   };
 
